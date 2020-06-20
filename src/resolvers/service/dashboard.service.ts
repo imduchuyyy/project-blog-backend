9e05f2a87099006c97c0708d01276f96e3aaa5be
Bùi Đức Huy <duchuy.124dk@gmail.com>
2020-06-20 23:38:37 +0700

    remove some import unnessesary

--- src/resolvers/service/dashboard.service.ts
+++ src/resolvers/service/dashboard.service.ts
@@ -1,25 +1,7 @@
-import {
-  Resolver,
-  Query,
-  Mutation,
-  Args,
-  Subscription,
-  Context,
-  ResolveProperty,
-  Parent
-} from '@nestjs/graphql'
 import { getMongoRepository } from 'typeorm'
-import {
-  ApolloError,
-  AuthenticationError,
-  ForbiddenError,
-  UserInputError
-} from 'apollo-server-core'
 import * as moment from 'moment'
-import { Gender, Role } from './../../generator/graphql.schema'
 import { UserEntity, PostEntity } from '@models';
 
-@Resolver()
 export class DashboardService {
   async dashboardUpdated(pubsub) {
     const users = await getMongoRepository(UserEntity).find({})
